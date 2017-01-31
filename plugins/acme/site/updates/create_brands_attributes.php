654e07c4d814bddb2b3650c5c0861ed204708262
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2017-01-31 18:37:17 +0100

    Update create_brands_attributes.php

--- plugins/acme/site/updates/create_brands_attributes.php
+++ plugins/acme/site/updates/create_brands_attributes.php
@@ -8,7 +8,7 @@ class CreateBrandsAttributesTable extends Migration
 {
     public function up()
     {
-        Schema::table('vojtasvoboda_brands_brands', function($table)
+        Schema::table('vojtasvoboda_brands_brands', function ($table)
         {
             $table->boolean('ceo')->default(false)->after('sort_order');
             $table->boolean('top')->default(false)->after('ceo');
